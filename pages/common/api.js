6445ca324c1c21211fd79c6e90cdcd57b1170696
cuikuikui <990417665@qq.com>
2022-12-15 10:25:37 +0800

    删除无用的接口定义

--- pages/common/api.js
+++ pages/common/api.js
@@ -1,68 +1,70 @@
-const getMainPage1 = '/WechatUserAdd' // 获取首页资源
-const getMainPage2 = '/WechatUserQuery' 
-const GetHospitalByCity='/GetHospitalByCity'
-const GetOfficeByHospitalId='/GetOfficeByHospitalId'
-const userServlet ='/userServlet'
-const GetRoomsByOfficeId='/GetRoomsByOfficeId'
-const scheduleList='/scheduleList'
-const GetDoctorsByRoomId='/GetDoctorsByRoomId'
-const GetDoctorsByDay='/GetDoctorsByDay'
-const AskerServlet='/AskerServlet'  //
-const OfficeServlet ='/OfficeServlet'
-const GuaHaoInsert='/GuaHaoInsert'
-const GetGuaHaoInfoByOpenId='/GetGuaHaoInfoByOpenId'
-const AddScheduleNum='/AddScheduleNum'
-const PayGuaHaoOK='/PayGuaHaoOK'
-const GetHospitalByName='/GetHospitalByName'
-const InsertCollection='/InsertCollection'
-const DeleteCollection='/DeleteCollection'
-const GetCollectHospitalByOpenid='/GetCollectHospitalByOpenid'
-const GetCollectDoctorByOpenid='/GetCollectDoctorByOpenid'
-const GetNoticeByHospitalId='/GetNoticeByHospitalId'
-const GetAllDoctorList='/GetAllDoctorList'
-const GetDoctorsByNamePercent='/GetDoctorsByNamePercent'
-const GetGuaHaoHospitals='/GetGuaHaoHospitals'
+// const getMainPage1 = '/WechatUserAdd' // 获取首页资源
+// const getMainPage2 = '/WechatUserQuery' 
+// const GetHospitalByCity='/GetHospitalByCity'
+// const GetOfficeByHospitalId='/GetOfficeByHospitalId'
+// const GetRoomsByOfficeId='/GetRoomsByOfficeId'
+// const GetDoctorsByRoomId='/GetDoctorsByRoomId'
+// const GetDoctorsByDay='/GetDoctorsByDay'
+// const GuaHaoInsert='/GuaHaoInsert'
+// const GetGuaHaoInfoByOpenId='/GetGuaHaoInfoByOpenId'
+// const AddScheduleNum='/AddScheduleNum'
+// const PayGuaHaoOK='/PayGuaHaoOK'
+// const GetHospitalByName='/GetHospitalByName'
+// const InsertCollection='/InsertCollection'
+// const DeleteCollection='/DeleteCollection'
+// const GetCollectHospitalByOpenid='/GetCollectHospitalByOpenid'
+// const GetCollectDoctorByOpenid='/GetCollectDoctorByOpenid'
+// const GetNoticeByHospitalId='/GetNoticeByHospitalId'
+// const GetAllDoctorList='/GetAllDoctorList'
+// const GetDoctorsByNamePercent='/GetDoctorsByNamePercent'
+// const GetGuaHaoHospitals='/GetGuaHaoHospitals'
+
+// const WechatUserUpdate='/WechatUserUpdate'
+// const DoctorOfficeRoomDaoServlet = '/DoctorOfficeRoomDaoServlet'
 const EvaluateAdd='/EvaluateAdd'
-const Questions_List_Json='/Questions_List_Json'
-const WechatUserUpdate='/WechatUserUpdate'
-const HospitalServlet ='/HospitalServlet'
-const NoticeServlet ='/NoticeServlet'
-const DoctorOfficeRoomDaoServlet = '/DoctorOfficeRoomDaoServlet'
 const RoomServlet = '/RoomServlet'
 const DoctorServlet ='/DoctorServlet'
 const ScheduleServlet ='/ScheduleServlet'
 const CollectionServlet ='/CollectionServlet'
 const GuaHaoServlet ='/GuaHaoServlet'
+const AskerServlet='/AskerServlet'  //
+const OfficeServlet ='/OfficeServlet'
+const scheduleList='/scheduleList'
+const userServlet ='/userServlet'
+const HospitalServlet ='/HospitalServlet'
+const NoticeServlet ='/NoticeServlet'
+const Questions_List_Json='/Questions_List_Json'
 //抛出getMainPage这个常量
 module.exports = {
-  getMainPage1,
-  getMainPage2,
-  GetHospitalByCity,
-  GetOfficeByHospitalId,
-  userServlet,
-  GetRoomsByOfficeId,
-  scheduleList,
-  GetDoctorsByRoomId,
-  GetDoctorsByDay,
+  // getMainPage1,
+  // getMainPage2,
+  // GetHospitalByCity,
+  // GetOfficeByHospitalId,
+
+  // GetRoomsByOfficeId,
+  // scheduleList,
+  // GetDoctorsByRoomId,
+  // GetDoctorsByDay,
+  // GuaHaoInsert,
+  // GetGuaHaoInfoByOpenId,
+  // AddScheduleNum,
+  // PayGuaHaoOK,
+  // GetHospitalByName,
+  // InsertCollection,
+  // DeleteCollection,
+  // GetCollectHospitalByOpenid,
+  // GetCollectDoctorByOpenid,
+  // GetNoticeByHospitalId,
+  // GetAllDoctorList,
+  // GetDoctorsByNamePercent,
+  // GetGuaHaoHospitals,
+  // DoctorOfficeRoomDaoServlet,
   AskerServlet,
-  GuaHaoInsert,
-  GetGuaHaoInfoByOpenId,
-  AddScheduleNum,
-  PayGuaHaoOK,
-  GetHospitalByName,
-  InsertCollection,
-  DeleteCollection,
-  GetCollectHospitalByOpenid,
-  GetCollectDoctorByOpenid,
-  GetNoticeByHospitalId,
-  GetAllDoctorList,
-  GetDoctorsByNamePercent,
-  GetGuaHaoHospitals,
+  userServlet,
   EvaluateAdd,
   Questions_List_Json,
   HospitalServlet,
   NoticeServlet,
-  DoctorOfficeRoomDaoServlet,
   OfficeServlet,
   RoomServlet,
   DoctorServlet,
