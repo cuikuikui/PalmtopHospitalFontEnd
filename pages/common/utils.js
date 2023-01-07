c6f964ddabf40607651ba39a9d67d35571ec8fda
cuikuikui <990417665@qq.com>
2023-01-07 20:22:23 +0800

    提交日期

--- pages/common/utils.js
+++ pages/common/utils.js
@@ -1,3 +1,6 @@
+// 定义一个函数
+import {ref,reactive,mounted} from 'vue'
+
 function randomNum(prefix,number){
 	// console.log("===prefix===",prefix)
 	// console.log("===number===",number)
@@ -8,4 +11,49 @@ function randomNum(prefix,number){
 	return prefix+""+ num
 }
 
-export {randomNum}
+//实时监听鼠标的动态
+function MouseMovement(){
+	// 定义默认的宽高
+	const movement = reactive({w:window.pageXOffset,h:window.pageYOffset});
+	mounted(()=>{
+		// 当窗口发生变化时候更新宽高
+		window.addEventListener("mousemove",function(){
+			movement.w = event.pageX;
+			movement.h = event.pageY;
+		})
+	})
+	// 返回size
+	return movement;
+}
+//获取当前日期函数yy-mm-dd
+function getNowFormatDate() {
+  let date = new Date(),
+    year = date.getFullYear(), //获取完整的年份(4位)
+    month = date.getMonth() + 1, //获取当前月份(0-11,0代表1月)
+    strDate = date.getDate() // 获取当前日(1-31)
+  if (month < 10) month = `0${month}` // 如果月份是个位数，在前面补0
+  if (strDate < 10) strDate = `0${strDate}` // 如果日是个位数，在前面补0
+ 
+  return `${year}-${month}-${strDate}`
+}
+
+//获取当前日期函数hh:mm:ss
+function getNowFormatTime() {
+  let date = new Date(),
+    obj = {
+      hour: date.getHours(), //获取当前小时(0 ~ 23)
+      minute: date.getMinutes(), //获取当前分钟(0 ~ 59)
+      second: date.getSeconds() //获取当前秒数(0 ~ 59)
+    }
+ 
+  Object.keys(obj).forEach(key => {
+    if (obj[key] < 10) obj[key] = `0${obj[key]}`
+    // console.log(obj[key])
+  })
+ 
+  return `${obj.hour}:${obj.minute}:${obj.second}`
+}
+export {randomNum,MouseMovement,getNowFormatDate,getNowFormatTime}
+
+
+
